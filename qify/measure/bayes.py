cc45520f199ec6f6c6d80f450f2e1c6402128332
Luigi D. C. Soares <luigidcsoares@gmail.com>
2024-01-08 18:51:51 -0300

    feat: added max case bayes vuln and leakage

--- qify/measure/bayes.py
+++ qify/measure/bayes.py
@@ -28,7 +28,11 @@ def prior(pi: ProbabDist) -> float:
     return pi.max();
 
 
-def posterior(pi: ProbabDist, ch: Channel) -> float:
+def posterior(
+    pi: ProbabDist,
+    ch: Channel,
+    max_case: bool = False
+) -> float:
     """
     Computes the posterior Bayes vulnerability of a channel `ch`,
     given a prior distribution `pi`. Bayes is essentially the
@@ -59,13 +63,29 @@ def posterior(pi: ProbabDist, ch: Channel) -> float:
     ...   ["00", "01", "10", "11"],
     ...   "password"
     ... )
+
+    We may be interested in the expected case:
     >>> posterior(pi, ch)
     0.8333333333333333
+
+    Or in the worst scenario possible:
+    >>> posterior(pi, ch, max_case=True)
+    1.0
     """
-    return ch.push_prior(pi).groupby(ch.output_names).max().sum()
+    joint = ch.push_prior(pi)
+    joint_grouped = joint.groupby(ch.output_names)
+    
+    if not max_case: return joint_grouped.max().sum()
+
+    outer_dist = joint_grouped.sum()
+    return joint.div(outer_dist).max()
 
 
-def mult_leakage(pi: ProbabDist, ch: Channel) -> float:
+def mult_leakage(
+    pi: ProbabDist,
+    ch: Channel,
+    max_case: bool = False
+) -> float:
     """
     Computes the multiplicative Bayes leakage of a channel `ch`,
     given a prior distribution `pi`.
@@ -92,11 +112,17 @@ def mult_leakage(pi: ProbabDist, ch: Channel) -> float:
     ... )
     >>> mult_leakage(pi, ch)
     2.5
+    >>> mult_leakage(pi, ch, max_case=True)
+    3.0
     """
-    return posterior(pi, ch) / prior(pi)
+    return posterior(pi, ch, max_case) / prior(pi)
 
 
-def add_leakage(pi: ProbabDist, ch: Channel) -> float:
+def add_leakage(
+    pi: ProbabDist,
+    ch: Channel,
+    max_case: bool = False
+) -> float:
     """
     Computes the additive Bayes leakage of a channel `ch`,
     given a prior distribution `pi`.
@@ -123,5 +149,7 @@ def add_leakage(pi: ProbabDist, ch: Channel) -> float:
     ... )
     >>> add_leakage(pi, ch)
     0.49999999999999994
+    >>> add_leakage(pi, ch, max_case=True)
+    0.6666666666666667
     """
-    return posterior(pi, ch) - prior(pi)
+    return posterior(pi, ch, max_case) - prior(pi)
